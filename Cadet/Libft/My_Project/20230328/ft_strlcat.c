d11a724477e2e2ebc08339d293b681e06737aa9d
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-03-30 21:58:18 +0700

    30032023

---
+++ Cadet/Libft/My_Project/20230328/ft_strlcat.c
@@ -0,0 +1,28 @@
+//#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = 0;
+	src_len = 0;
+	i = 0;
+	while (dst[dst_len])
+		dst_len++;
+	while (src[src_len])
+		src_len++;
+	if (dstsize - 1 > dst_len && dstsize > 0)
+	{
+		while (src[i] && dst_len + i < dstsize - 1)
+		{
+			dst[dst_len + i] = src[i];
+			i++;
+		}
+		dst[dst_len + i] = 0;
+	}
+	if (dstsize <= dst_len)
+		dst_len = dstsize;
+	return (dst_len + src_len);
+}
