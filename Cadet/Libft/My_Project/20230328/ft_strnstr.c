d0304b5b30172dd275b7d3052cc24b28ab80fe96
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-03-31 19:38:55 +0700

    31032023

---
+++ Cadet/Libft/My_Project/20230328/ft_strnstr.c
@@ -0,0 +1,27 @@
+//#include "libft.h"
+#include "stdio.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!needle[0])
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && needle[j] && haystack[i + j] == needle[j] && i + j < len)
+			j++;
+		if (!needle[j])
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
+
+int	main()
+{
+	char a[] = "abcdef";
+	char b[] =
