5ec7b67c167e9220d4fc17484516c88cacf5a4d1
Nathavorn Tharavorarut <natharav@z1t5c3.42bangkok.com>
2023-04-01 19:07:21 +0700

    add calloc & strdup

--- Cadet/Libft/My_Project/20230328/ft_calloc.c
+++ Cadet/Libft/My_Project/20230328/ft_calloc.c
@@ -1,34 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: natharav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/07 15:07:24 by ncolomer          #+#    #+#             */
-/*   Updated: 2023/04/01 18:58:41 by natharav         ###   ########.fr       */
+/*   Created: 2023/04/01 18:28:20 by natharav          #+#    #+#             */
+/*   Updated: 2023/04/01 18:35:22 by natharav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char
-	*ft_strdup(const char *s1)
+void	*ft_calloc(size_t count, size_t size)
 {
-	char	*str;
-	size_t	i;
+	void	*ptr;
 
-	if (!s1)
+	ptr = (void *)malloc(count * size);
+	if (!ptr)
 		return (NULL);
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	str[i] = 0;
-	return (str);
+	ft_bzero(ptr, count);
+	return (ptr);
 }
--- Cadet/Libft/My_Project/20230328/ft_strdup.c
+++ Cadet/Libft/My_Project/20230328/ft_strdup.c
@@ -3,27 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ncolomer <ncolomer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: natharav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/07 15:07:24 by ncolomer          #+#    #+#             */
-/*   Updated: 2023/04/01 18:58:41 by natharav         ###   ########.fr       */
+/*   Created: 2023/04/01 18:38:47 by natharav          #+#    #+#             */
+/*   Updated: 2023/04/01 19:04:06 by natharav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char
-	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
 	char	*str;
 	size_t	i;
 
+	i = 0;
 	if (!s1)
 		return (NULL);
 	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
 	if (!str)
 		return (NULL);
-	i = 0;
 	while (s1[i])
 	{
 		str[i] = s1[i];
