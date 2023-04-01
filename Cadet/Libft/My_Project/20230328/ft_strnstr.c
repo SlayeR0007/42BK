3cfe4b5fc9abc1e0287594727417458dd18ffdc0
Nathavorn Tharavorarut <natharav@z1t5c3.42bangkok.com>
2023-04-01 13:54:35 +0700

    revise norm

--- Cadet/Libft/My_Project/20230328/ft_strnstr.c
+++ Cadet/Libft/My_Project/20230328/ft_strnstr.c
@@ -1,10 +1,22 @@
-//#include "libft.h"
-#include "stdio.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: natharav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 13:23:42 by natharav          #+#    #+#             */
+/*   Updated: 2023/04/01 13:53:36 by natharav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	k;
 
 	i = 0;
 	if (!needle[0])
@@ -12,16 +24,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] && needle[j] && haystack[i + j] == needle[j] && i + j < len)
+		k = (haystack[i + j] == needle[j]);
+		while (haystack[i + j] && needle[j] && k && i + j < len)
+		{
+			k = (haystack[i + j] == needle[j]);
 			j++;
+		}
 		if (!needle[j])
 			return ((char *)(haystack + i));
 		i++;
 	}
 	return (NULL);
 }
-
-int	main()
-{
-	char a[] = "abcdef";
-	char b[] =
