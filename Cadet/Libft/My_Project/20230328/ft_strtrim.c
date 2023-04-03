2623a0dae1b258a429aec471ca6d33c10d346936
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-04-03 20:02:03 +0700

    debug ft_strtrim

--- Cadet/Libft/My_Project/20230328/ft_strtrim.c
+++ Cadet/Libft/My_Project/20230328/ft_strtrim.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim01.c                                     :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: natharav <umm_mvp@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 20:05:39 by natharav          #+#    #+#             */
-/*   Updated: 2023/04/02 21:18:11 by natharav         ###   ########.fr       */
+/*   Updated: 2023/04/03 20:00:47 by natharav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 */
 
 #include "libft.h"
-#include <stdio.h>
 
 static char	*empty_string(void)
 {
@@ -50,22 +49,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	s1_len = s1_len - 1;
 	while (s1[s1_len] && ft_strchr(set, s1[s1_len]))
 	{
-		printf("%d\n", s1_len);
-		printf("ft : %s\n", ft_substr(s1, i, s1_len));
 		s1_len--;
 	}
-	printf("-----------\n%d\n", s1_len);
-		return (ft_substr(s1, i, s1_len - i + 2));
-}
-
-int	main()
-{
-	char	a[] = "1234ac789012345";
-	char	b[] = "0123456789";
-	char	*ptr;
-
-	ptr = ft_strtrim(a, b);
-	printf("ptr : %s\n", ptr);
-	free(ptr);
-	return (0);
+		return (ft_substr(s1, i, s1_len - i + 1));
 }
