74d4f079bd2141f41b1879d1317ad8af7a0aa267
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-04-10 00:08:34 +0700

    Finish part02

--- Cadet/Libft/My_Project/20230328/ft_putnbr_fd.c
+++ Cadet/Libft/My_Project/20230328/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: natharav <umm_mvp@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 20:49:26 by natharav          #+#    #+#             */
-/*   Updated: 2023/04/09 20:57:18 by natharav         ###   ########.fr       */
+/*   Updated: 2023/04/10 00:07:12 by natharav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,26 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	int	temp;
+
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	if (n < 0)
 	{
-	 	write(fd, '-', 1);
+	 	write(fd, "-", 1);
 		n *= -1;
 	}
-	while (n)
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		temp = n % 10 + '0';
+		write(fd, &temp, 1);
+	}
 }
