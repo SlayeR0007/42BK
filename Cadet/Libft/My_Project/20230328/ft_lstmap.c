25267d89395174ad98ca0c2d6cc9f8efa5152acf
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-04-13 15:52:12 +0700

    Checkpoint of finishing part 3

---
+++ Cadet/Libft/My_Project/20230328/ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: natharav <umm_mvp@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 15:10:01 by natharav          #+#    #+#             */
+/*   Updated: 2023/04/13 15:43:52 by natharav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*begin;
+	t_list	*new;
+
+	if (!f || !del)
+		return (NULL);
+	begin = NULL;
+	while (lst)
+	{
+		if (!(new = ft_lstnew((*f)(lst->content))))
+		{
+			ft_lstclear(&begin, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&begin, new);
+		lst = lst->next;
+	}
+	return (begin);
+}
