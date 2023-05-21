b8ddf7bcc810c8856ae79c076dcf35e611470637
Nathavorn Tharavorarut <Nathavorn.Off@gmail.com>
2023-05-22 00:06:02 +0700

    Finish_get_next_line_20230521

--- Cadet/get_next_line/My_Project/20230520/main.c
+++ Cadet/get_next_line/My_Project/20230520/main.c
@@ -6,7 +6,7 @@
 /*   By: <natharav> <Umm_MVP@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 16:40:22 by jphonyia          #+#    #+#             */
-/*   Updated: 2023/05/21 16:36:08 by <natharav>       ###   ########.fr       */
+/*   Updated: 2023/05/22 00:04:59 by <natharav>       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,36 @@
 # include <fcntl.h>
 # include "get_next_line.h"
 
-int	main()
+int	main(void)
 {
-	printf("=====hello========\n");
-	int fd = open("tester.txt", O_RDONLY);
+	int		fd;
+	char	*str;
+	char	buff[10000];
+	int		byte;
+
+	fd = open("tester.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Cannot open file\n");
 		return (1);
 	}
-	printf("=====fd===========\nfd : %d\n",fd);
-	char buff[1111111];
-	int byte = read(fd, buff, 8);
-	printf("=====byte=========\nbyte : %d\n",byte);
-	printf("=====bye-bye======\n");
-	printf("\n");
 
-	char *str = get_next_line(fd);
+	byte = read(fd, buff, 3);
+	printf("byte : %d\n", byte);
+
+	str = get_next_line(fd);
 	printf("%s",str);
 	printf("%s",get_next_line(fd));
 	printf("%s",get_next_line(fd));
-
-	// printf("%s--\n", str);
-	// while (1)
-	// {
-	// 	//printf("%i--:while (s)\n", fd);
-	// 	char *str = get_next_line(fd);
-	// 	if (!str)
-	// 		break ;
-	// 	printf("%s",str);
-	// }
-
+	str = get_next_line(fd);
+	printf("%s",str);
+	str = get_next_line(fd);
+	printf("%s",str);
+	str = get_next_line(fd);
+	printf("%s",str);
+	str = get_next_line(fd);
+	printf("%s",str);
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
 	return (0);
 }
